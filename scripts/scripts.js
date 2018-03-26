8d9c8c6b8b96d5dadd61d018667c03ddee67b0d9
Liam McColgan <l.mccolgan.connect@gmail.com>
2018-03-26 17:12:29 +0100

    added sections and showcase js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
         $('.nav-outer').toggleClass('nav-outer--open', 400);
         $(this).toggleClass('open');
     });
+
+    // Smooth Scrolling
+    var scrollLink = $('.scroll');
+    scrollLink.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+        scrollTop: $(this.hash).offset().top - $('.header-outer').outerHeight()
+        }, 1500);
+    });
 });
